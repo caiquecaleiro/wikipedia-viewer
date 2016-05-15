aff33a65e2d61a5af2e2288fb6b9d5aa91ace940
Caique Caleiro <caiquecaleiro@gmail.com>
2016-05-15 18:14:59 +0530

    Add viewer controller

---
+++ src/app/viewer/viewer.controller.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app.viewer')
+    .controller('ViewerController', ViewerController);
+
+    function ViewerController() {
+      var vm = this;
+    }
+})();
